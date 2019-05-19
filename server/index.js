03c5be3d355e33382d0bd8048e1689503afbb155
mawarijo <mdopure@gmail.com>
2019-05-19 10:39:22 -0700

    make call to external API from route

--- server/index.js
+++ server/index.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const request = require("request");
 
 const app = express();
 
-app.listen(3000);
+app.get("/", async (req, res) => {
+  request("https://data.nasa.gov/resource/gh4g-9sfh.json", function(
+    error,
+    response,
+    body
+  ) {
+    if (!error && response.statusCode === 200) {
+      res.json(JSON.parse(body));
+    } else {
+      res.json(error);
+    }
+  });
+});
+
+app.listen(4000);
