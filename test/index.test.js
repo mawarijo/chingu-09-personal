086cb8435d5cb1270a2469e0af1cef3cb4ade3d5
mawarijo <mdopure@gmail.com>
2019-05-27 21:23:11 -0700

    add back end tests

--- test/index.test.js
+++ test/index.test.js
@@ -6,12 +6,37 @@ var expect = chai.expect;
 
 chai.use(chaiHttp);
 
-it("responds with status 200", function(done) {
+it("fetches landing data", function(done) {
   chai
     .request(app)
     .get("/api/landings")
     .end(function(err, res) {
       expect(res).to.have.status(200);
+      expect(res.body).to.have.lengthOf(100);
+      done();
+    });
+});
+
+it("fetches filtered results", function(done) {
+  chai
+    .request(app)
+    .get("/api/landings")
+    .query({ query: "aa" })
+    .end(function(err, res) {
+      expect(res).to.have.status(200);
+      expect(res.body).to.have.lengthOf(12);
+      done();
+    });
+});
+
+it("fetches filtered results regardless of case and whitespace", function(done) {
+  chai
+    .request(app)
+    .get("/api/landings")
+    .query({ query: "aA  " })
+    .end(function(err, res) {
+      expect(res).to.have.status(200);
+      expect(res.body).to.have.lengthOf(12);
       done();
     });
 });
