783519094a919d8d44c4c13059a59b6315cb6ebe
mawarijo <mdopure@gmail.com>
2019-05-27 20:56:56 -0700

    fix query to make case insensitive

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 4000;
 app.get("/api/landings", async (req, res) => {
   const offset = req.query.offset ? req.query.offset : 0;
   const query = req.query.query ? req.query.query : "";
-  const url = `https://data.nasa.gov/resource/gh4g-9sfh.json?$order=name&$limit=100&$offset=${offset}&$where=name like '%25${query}%25'`;
+  const url = `https://data.nasa.gov/resource/gh4g-9sfh.json?$order=name&$limit=100&$offset=${offset}&$where=lower(name) like lower('%25${query.trim()}%25')`;
 
   request(url, function(error, response, body) {
     if (!error && response.statusCode === 200) {
